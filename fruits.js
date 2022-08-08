dfdd4c26f8d2f4175deb64812337f71a237ab3ea
Ishita L <79676329+NPC-Bl00d1@users.noreply.github.com>
2022-08-08 08:25:10 -0600

    Add files via upload

--- fruits.js
+++ fruits.js
@@ -4,7 +4,7 @@ var status = "";
 
 function preload(){
 
-img = loadImage('bsd by beloved.jpg');
+img = loadImage('bedroom.jpg');
 
 }
 
@@ -24,6 +24,33 @@ function modelLoaded(){
     objectdetector.detect(img, gotResult);
     
     }
+   
+    
+    console.log(results);
+    objects = results;
+    
+    
+    function draw(){
+
+        image(img, 0, 0, 640, 420);
+        if(status != ""){
+        
+        for(i = 0; i < objects.length; i++){
+        
+        document.getElementById("status").innerHTML = "Status: Object(s) Detected";
+        
+        fill("#7f58c7");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+        noFill();
+        stroke("#7f58c7");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        
+        }
+        
+        }
+        
+        }
     
     function gotResult(error, results){
     
